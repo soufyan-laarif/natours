8cfce059a35b0b4a4aa26a468682c1be9a036295
Soufian2021 <soufyanlaarif7@gmail.com>
2024-01-24 13:35:13 +0100

    Initialize API with basic CRUD functionality

---
+++ app.js
@@ -0,0 +1,101 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+// the middleware
+app.use(express.json());
+
+// app.get('/', (req, res) => res.status(200).send('Hello from the server side!'));
+// app.get('/', (req, res) =>
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' })
+// );
+
+// app.post('/', (req, res) => res.send('You can post to this endpoint'));
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = +req.params.id;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID !',
+    });
+  }
+
+  res.status(200).json({
+    status: 'sucess',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body, typeof req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { id: newId, ...req.body };
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'sucess',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID !',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    message: '<Updated tour here ...>',
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID !',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    message: null,
+  });
+});
+
+const port = 3000;
+app.listen(port, () => console.log(`App running on port ${port} ...`));
