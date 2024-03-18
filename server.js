150f988200ec1076139304079808feaab1ffa68b
Soufian2021 <soufyanlaarif7@gmail.com>
2024-03-18 23:45:39 +0000

    Read and save the config.env file to be available globally in the process and using the pre-configured port env variable

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+const dotenv = require('dotenv');
 const app = require('./app');
 
+dotenv.config({ path: './config.env' });
+
+// console.log(process.env);
+
 // START SERVER
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App running on port ${port} ...`));
